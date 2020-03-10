bc6c4ecc612e7fdaea0d1b5a5fc8d2a119f255e9
Niclas <niclasbangman@gmail.com>
2020-03-10 21:55:28 +0100

    add-review: basic form error messages, add menuType and icons

--- src/AddReviewPage.js
+++ src/AddReviewPage.js
@@ -6,6 +6,8 @@ import {
 } from "@material-ui/core";
 import Autocomplete from "@material-ui/lab/Autocomplete";
 import SaveIcon from "@material-ui/icons/Save";
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import Explore from '@material-ui/icons/Explore';
 import { svSE } from "@material-ui/core/locale";
 import { firstLetterUpperCase } from "./utils.js"
 import { getRestaurantMeta, getRestaurantReviews, addReview } from "./api.js"
@@ -27,13 +29,13 @@ const potionSizeOptions = ["Lite", "Under medel", "Medel", "Över medel", "Mycke
 const waitTimeOptions = ["< 5 min", "< 10 min", "< 20 min", "< 30 min", "> 30 min"];
 const defaultState = {
   newRestaurant:false, newMeal:false, description:"", price:"", portionSize: "Medel",
-  meal:"", restaurant:"", address:"", website:"", seats:null, reviewError:false,
+  meal:"", restaurant:"", address:"", website:"", seats:null, timestamp:new Date().toISOString().slice(0, 10),
   review:"", restaurantComment:"", tasteScore:5, environmentScore:5, innovationScore:5,
-  extrasScore:5, heat:"Ingen hetta", waitTime:"< 20 min", payInAdvance:"Ja", timestamp:new Date().toISOString().slice(0, 10)
+  extrasScore:5, heat:"Ingen hetta", waitTime:"< 20 min", payInAdvance:"Ja", menuType: "other"
 };
 
 const saveWhiteList = [
-  "tasteScore", "environmentScore", "meal", "description", "price",
+  "tasteScore", "environmentScore", "meal", "description", "price", "menuType",
   "totalPrice", "portionSize", "heat", "waitTime", "totalTime", "extrasScore", 
   "innovationScore", "restaurantComment", "review", "timestamp"
 ]
@@ -184,10 +186,10 @@ const NewRestaurant = function(props) {
         <SimpleSelect id="pay-in-advance" label="Betalade i förväg" value={props.payInAdvance} onChange={props.updatePayInAdvance} options={["Ja", "Nej"]}/>
       </GridRow>
       <GridRow>
-        <TextField required value={props.address} onChange={props.updateAddress} error={props.addressError} id="restaurant-address-field" label="Address" style={{width: "50vw", margin: 10}} />
+        <TextField required value={props.address} onChange={props.updateAddress} error={!!props.addressError} helperText={props.addressError} id="restaurant-address-field" label="Address" style={{width: "50vw", margin: 10}} InputProps={{endAdornment: <InputAdornment position="end"><LocationOnIcon /></InputAdornment>}} />
       </GridRow>
       <GridRow>
-        <TextField required value={props.website} onChange={props.updateWebsite} error={props.websiteError}id="restaurant-website-field" label="Hemsida" style={{width: "50vw", margin: 10}} />
+        <TextField required value={props.website} onChange={props.updateWebsite} error={!!props.websiteError} helperText={props.websiteError} id="restaurant-website-field" label="Hemsida" style={{width: "50vw", margin: 10}} InputProps={{endAdornment: <InputAdornment position="end"><Explore /></InputAdornment>}} />
       </GridRow>
       <GridRow>
         <TagSelect onChange={props.updateTags} />
@@ -205,19 +207,30 @@ const NewRestaurant = function(props) {
 const NewMeal = props => (
   <>
     <h4 style={{marginTop: 20}}>Definera ny måltid</h4>
-    <TextField required error={props.descError} value={props.desc} id="meal-description-field" label="Beskrivning" style={{width: "50vw", marginBottom: 30}} onChange={props.updateDesc}/>
+    <TextField required error={!!props.error} helperText={props.descError} value={props.desc} id="meal-description-field" label="Beskrivning" style={{width: "50vw", marginBottom: 30}} onChange={props.updateDesc}/>
   </>
 );
 
 const RestaurantSeats = props => (
-  <div className="py-2">
-    <FormLabel id="seats-range-slider" component="legend">Sittplatser</FormLabel>
+  <div className="mt-2">
+    <FormLabel id="seats" component="legend">Sittplatser</FormLabel>
     <RadioGroup row aria-label="seats" name="Seats" value={props.seats || "25-35"} onChange={props.updateSeats}>
       <FormControlLabel value="<15" control={<Radio />} label="<15" />
       <FormControlLabel value="15-25" control={<Radio />} label="15-25" />
       <FormControlLabel value="25-35" control={<Radio />} label="25-35" />
       <FormControlLabel value="35-50" control={<Radio />} label="35-50" />
       <FormControlLabel value=">50" control={<Radio />} label=">50" />
+    </RadioGroup>
+  </div>
+)
+
+const MenuType = props => (
+  <div className="mt-2">
+    <FormLabel id="menu-type" component="legend">Rättens menu-typ</FormLabel>
+    <RadioGroup row aria-label="menu-type" name="Seats" value={props.menuType || "other"} onChange={props.updateMenuType}>
+      <FormControlLabel value="daily" control={<Radio />} label="Dagens rätt" />
+      <FormControlLabel value="weekly" control={<Radio />} label="Veckans rätt" />
+      <FormControlLabel value="other" control={<Radio />} label="Annan" />
     </RadioGroup>
   </div>
 )
@@ -257,7 +270,7 @@ const ReviewDate = props => (
 )
 
 const MealSelect = function(props) {
-  return <ComboBox value={props.meal} error={props.error} label="Måltid" id="meal" options={props.meals} addNew={props.addNew} onChange={props.updateMeal} />;
+  return <ComboBox value={props.meal} error={!!props.error} helperText={props.error} label="Måltid" id="meal" options={props.meals} addNew={props.addNew} onChange={props.updateMeal} />;
 }
 
 const SimpleSelect = props => (
@@ -304,17 +317,24 @@ export default class AddReviewPage extends Component {
         this.setState({newMeal: show});
       }
     }
-    this.updateMeal = (event, value) => this.setState({meal: firstLetterUpperCase(value), mealError:false});
-    this.updateNewMealDesc = (event) => this.setState({description: firstLetterUpperCase(event.target.value), descriptionError:false});
-    this.updateWebsite = (event) => this.setState({website: event.target.value, websiteError:false});
-    this.updateAddress = (event) => this.setState({address: firstLetterUpperCase(event.target.value), addressError:false});
+    this.updateMeal = (event, value) => this.setState({meal: firstLetterUpperCase(value), mealError: ""});
+    this.updateNewMealDesc = (event) => this.setState({description: firstLetterUpperCase(event.target.value), descriptionError: ""});
+    this.updateWebsite = (event) => {
+      var websiteError = "";
+      if (!event.target.value.match(/^https?:\/\/(www\.)?[\w\d]+\.[\w]+(\/[\w\d]+)*$/g)) {
+        websiteError = "Invalid URL (has to start with http and can't end with a slash)";
+      }
+      this.setState({website: event.target.value, websiteError: websiteError});
+    }
+    this.updateAddress = (event) => this.setState({address: firstLetterUpperCase(event.target.value), addressError: ""});
     this.updateSeats = (event, value) => this.setState({seats: value});
+    this.updateMenuType = (event, value) => this.setState({menuType: value});
     this.updateDate = (event, value) => this.setState({timestamp: value});
     this.updatePrice = (event) => this.setState({price: parseInt(event.target.value.replace(/[^0-9,]/g, "") || 0)});
     this.updateTaste = (event, value) => this.setState({tasteScore: value});
     this.updateExtras = (event, value) => this.setState({extrasScore: value});
     this.updateInnovation = (event, value) => this.setState({innovationScore: value});
-    this.updateReview = (event) => this.setState({review: firstLetterUpperCase(event.target.value), reviewError:false});
+    this.updateReview = (event) => this.setState({review: firstLetterUpperCase(event.target.value), reviewError: ""});
     this.updateComment = (event) => this.setState({restaurantComment: firstLetterUpperCase(event.target.value)});
     this.updateEnviroment = (event, value) => this.setState({environmentScore: value});
     this.updateHeat = (event) => this.setState({heat: event.target.value});
@@ -335,7 +355,7 @@ export default class AddReviewPage extends Component {
 
   updateRestaurant(event, value) {
     const { restaurantMeta } = this.state;
-    this.setState({restaurant: value, restaurantError: false})
+    this.setState({restaurant: value, restaurantError: ""})
 
     restaurantMeta.forEach(restaurant => {
       if (value.toLowerCase() === restaurant.name.toLowerCase()) {
@@ -369,26 +389,26 @@ export default class AddReviewPage extends Component {
       return true;
     }
     if (!meal) {
-      this.setState({"mealError": true});
+      this.setState({"mealError": "Missing value"});
     }
     if (!price) {
-      this.setState({"priceError": true});
+      this.setState({"priceError": "Missing value"});
     }
     if (!review) {
-      this.setState({"reviewError": true});
+      this.setState({"reviewError": "Missing value"});
     }
     if (!restaurant) {
-      this.setState({"restaurantError": true});
+      this.setState({"restaurantError": "Missing value"});
     }
     if (isNewRestaurant) {
       if (!description) {
-        this.setState({"descriptionError": true});
+        this.setState({"descriptionError": "Missing value"});
       }
       if (!website) {
-        this.setState({"websiteError": true});
+        this.setState({"websiteError": "Missing value"});
       }
       if (!address) {
-        this.setState({"addressError": true});
+        this.setState({"addressError": "Missing value"});
       }
     }
     return false;
@@ -415,7 +435,7 @@ export default class AddReviewPage extends Component {
       restaurantMeta, seats, restaurant, newRestaurant, meals, newMeal, description, meal, mealError,
       restaurantError, descriptionError, website, websiteError, address, addressError, tasteScore, heat,
       review, reviewError, environmentScore, restaurantComment, innovationScore, price, priceError,
-      portionSize, extrasScore, waitTime, payInAdvance, username, timestamp
+      portionSize, extrasScore, waitTime, payInAdvance, username, timestamp, menuType
     } = this.state;
     const restaurants = List(restaurantMeta).map(meta => meta.name).toArray();
 
@@ -426,7 +446,7 @@ export default class AddReviewPage extends Component {
           <h1 className="page-header text-center">Recensera</h1>
           <Grid container spacing={2} >
             <GridRow>
-              <RestaurantSelect restaurant={restaurant} error={restaurantError} restaurants={restaurants} updateRestaurant={this.updateRestaurant} addNew={this.toggleNewRestaurant}/>
+              <RestaurantSelect restaurant={restaurant} error={!!restaurantError} helperText={restaurantError} restaurants={restaurants} updateRestaurant={this.updateRestaurant} addNew={this.toggleNewRestaurant}/>
             </GridRow>
             <GridRow collapse={!newRestaurant}>
               <NewRestaurant website={website} websiteError={websiteError} updateWebsite={this.updateWebsite} address={address} addressError={addressError} updateAddress={this.updateAddress} 
@@ -436,15 +456,18 @@ export default class AddReviewPage extends Component {
               <MealSelect meal={meal} error={mealError} updateMeal={this.updateMeal} meals={Object.keys(meals)} addNew={this.toggleNewMeal}/>
             </GridRow>
             <GridRow collapse={!newMeal}>
-              <NewMeal desc={description} descError={descriptionError} updateDesc={this.updateNewMealDesc} />
+              <NewMeal desc={description} updateDesc={this.updateNewMealDesc} error={descriptionError} />
             </GridRow>
           </Grid>
           <Grid container spacing={2} >
             <GridRow>
               <ReviewDate value={timestamp} updateDate={this.updateDate} />
+            </GridRow>
+            <GridRow>
+              <MenuType menuType={menuType} updateMenuType={this.updateMenuType}/>
             </GridRow>  
             <GridRow>
-              <TextField required value={price} onChange={this.updatePrice} error={priceError} id="price-field" label="Pris" style={{width: "50vw", margin:10}} InputProps={{endAdornment: <InputAdornment position="end">kr</InputAdornment>}} />
+              <TextField required value={price} onChange={this.updatePrice} error={!!priceError} helperText={priceError} id="price-field" label="Pris" style={{width: "50vw", margin:10}} InputProps={{endAdornment: <InputAdornment position="end">kr</InputAdornment>}} />
             </GridRow>
             <GridRow>
               <SimpleSelect id="wait-time" label="Tid innan servering" value={waitTime} onChange={this.updateWaitTime} options={waitTimeOptions}/>
@@ -471,7 +494,7 @@ export default class AddReviewPage extends Component {
               <TextField value={restaurantComment} onChange={this.updateComment} id="comment-field" label="Restaurang kommentar" style={{width: "50vw", margin: 10}} />
             </GridRow>
             <GridRow>
-              <TextField required value={review} onChange={this.updateReview} error={reviewError} id="review-field" label="Måltids recension" style={{width: "50vw", margin: 10}} />
+              <TextField required value={review} onChange={this.updateReview} error={!!reviewError} helperText={reviewError} id="review-field" label="Måltids recension" style={{width: "50vw", margin: 10}} />
             </GridRow>
             <GridRow>
               <SaveButton state={this.state} clear={this.clear} validate={this.validateFields} />
